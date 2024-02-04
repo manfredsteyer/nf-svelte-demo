8bf6896c593c9e555620a37cd3dd36216a349615
Manfred Steyer <manfred.steyer@gmx.net>
2024-02-04 21:48:57 +0100

    remove styles to favor styles provided by shell

--- remote/src/App.svelte
+++ remote/src/App.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
-	import { of, tap } from 'rxjs';
+  import { of, tap } from "rxjs";
 
-	import { state } from 'shared';
-	console.log('remote got message:', state.message);
+  import { state } from "shared";
+  console.log("remote got message:", state.message);
 
-	of('emit')
-		.pipe(tap(() => console.log("I'm RxJs from remote")))
-		.subscribe();
+  of("emit")
+    .pipe(tap(() => console.log("I'm RxJs from remote")))
+    .subscribe();
 </script>
 
 <div id="container">
-    <div>
-        <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/svelte-icon.png" alt="svelte" width="50">
+  <div>
+    <img
+      src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/svelte-icon.png"
+      alt="svelte"
+      width="50"
+    />
+  </div>
+  <h1>Select an Upgrade</h1>
+    <div class="form-group">
+      <label for="from">From:</label>
+      <input id="from" name="from" class="form-control" />
     </div>
-    <h1>Select an Upgrade</h1>
-    <div>
-        <input type="text" placeholder="From">
+    <div class="form-group">
+      <label for="to">To:</label>
+      <input id="to" name="to" class="form-control" />
     </div>
-    <div>
-        <input type="text" placeholder="To">
-    </div>
-    <div>
-        <button>Search</button>
+
+    <div class="form-group">
+      <button class="btn btn-default">Search</button>
     </div>
 </div>
 
 <style>
+  /*
 input[type=text] {
     width: 100%;
     max-width: 400px;
@@ -53,9 +61,9 @@ button {
 button:active {
     border-color: black;
 }
-
-#container {
+*/
+  #container {
     border: 2px orange dashed;
     padding: 20px;
-}
+  }
 </style>
